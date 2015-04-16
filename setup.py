94a9a191967706e2ed10855da96ecd5845989a7b
Josh Cartmell <joshcartme@gmail.com>
2015-04-15 21:03:40 -0400

    Fix setup.py so it also install templates.

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import find_packages
 setup(
     name = 'mezzanine-modal-announcements',
     packages = find_packages(),
-    version = '1.1.1',
+    include_package_data=True,
+    version = '1.1.2',
     description = 'Popup announcements for Mezzanine websites utilizing Bootstrap modals.',
     author = 'Joshua Cartmell',
     author_email = 'chat@joshc.io',
     url = 'https://github.com/joshcartme/mezzanine-modal-announcements',
-    download_url = 'https://github.com/joshcartme/mezzanine-modal-announcements/tarball/1.1.1',
+    download_url = 'https://github.com/joshcartme/mezzanine-modal-announcements/tarball/1.1.2',
     keywords = ['mezzanine', 'django', 'announcements'],
     classifiers = [],
 )
